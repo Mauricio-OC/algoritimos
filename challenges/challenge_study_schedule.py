05dc5511c131cade14b99ee4c58c5b973d3749e5
Mauricio-OC <murisc0@hotmail.com>
2023-08-19 23:10:40 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or any(
+        start > end for start, end in permanence_period
+    ):
+        return None
+
+    return sum(
+        1 for start, end in permanence_period if start <= target_time <= end)
