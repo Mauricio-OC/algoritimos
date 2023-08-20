0a479bca6673e75be8fdb39fb60a6a237749a025
Mauricio-OC <murisc0@hotmail.com>
2023-08-20 01:10:33 -0300

    anagrama req

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
+def ana_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = first_string.replace(" ", "").lower()
+    second_string = second_string.replace(" ", "").lower()
+
+    if len(first_string) != len(second_string):
+        return first_string, second_string, False
+
+    first_list = list(first_string)
+    second_list = list(second_string)
+
+    ana_sort(first_list)
+    ana_sort(second_list)
+
+    return "".join(first_list), "".join(second_list), first_list == second_list
